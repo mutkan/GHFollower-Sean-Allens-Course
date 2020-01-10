f0bf4f6911e68c1febb8d38bfca37fe625dc73a9
Roberto Halgravez <roberto.halgravez@janrain.com>
2020-01-09 21:35:59 -0700

    Create project
    Also delete main storyboard form project and plist

---
+++ GHFollower/GHFollower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 1/9/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
