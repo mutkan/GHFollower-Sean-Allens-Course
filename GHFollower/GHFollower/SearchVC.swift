35796a706ded7e9b99b4edb5f42df482cc56853c
Roberto Halgravez <r.halgravez@gmail.com>
2020-01-16 16:54:11 -0700

    Add a new class called FollowerListVC that is going to be use to show the result from a search. Add an action to the callToActionButton to show the follower list VC Make the SearchVC to conform to the UITextFieldDelegate so the "go" key can show the follower list screen (not only the button)

--- GHFollower/GHFollower/SearchVC.swift
+++ GHFollower/GHFollower/SearchVC.swift
@@ -31,6 +31,14 @@ class SearchVC: UIViewController {
     }
     
     
+    @objc func pushFollowerListVC() {
+        let followerListVC = FollowerListVC()
+        followerListVC.username = usernameTextField.text
+        followerListVC.title    = usernameTextField.text
+        navigationController?.pushViewController(followerListVC, animated: true)
+    }
+    
+    
     func createDismissKeyboardTapGesture() {
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
@@ -53,6 +61,7 @@ class SearchVC: UIViewController {
     
     func configureTextField() {
         view.addSubview(usernameTextField)
+        usernameTextField.delegate = self
         
         NSLayoutConstraint.activate([
             usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
@@ -65,6 +74,7 @@ class SearchVC: UIViewController {
     
     func configureCallToActionButton() {
         view.addSubview(callToActionButton)
+        callToActionButton.addTarget(self, action: #selector(pushFollowerListVC), for: .touchUpInside)
         
         NSLayoutConstraint.activate([
             callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
@@ -74,4 +84,12 @@ class SearchVC: UIViewController {
         ])
     }
 
+}
+
+
+extension SearchVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pushFollowerListVC()
+        return true
+    }
 }
