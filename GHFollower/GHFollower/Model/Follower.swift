15723126d41446494c6a816bd03004cd475503eb
Roberto Halgravez <r.halgravez@gmail.com>
2020-01-20 23:18:25 -0700

    Create the struct Follower, this will work as our model to get the Followers from user with their username and avatar picture url

---
+++ GHFollower/GHFollower/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 1/20/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
