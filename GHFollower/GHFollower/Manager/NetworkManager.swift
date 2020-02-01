7778d28597f0143ef5a6de53fba5f4095b29c095
Roberto Halgravez <r.halgravez@gmail.com>
2020-01-31 19:11:12 -0700

    Create a new folder called "Manager" and also created the NetworkManager Class

---
+++ GHFollower/GHFollower/Manager/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 1/31/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+}
