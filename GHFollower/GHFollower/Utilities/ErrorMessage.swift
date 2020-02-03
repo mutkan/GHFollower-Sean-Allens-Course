f019428aef2f7a9f6f40be06fde6b34a48ce082d
Roberto Halgravez <r.halgravez@gmail.com>
2020-02-02 18:14:43 -0700

    Refactor the NetworkManager class to use the new ErrorMessage enum to have the error messages in one place and can be reused

--- GHFollower/GHFollower/Utilities/ErrorMessage.swift
+++ GHFollower/GHFollower/Utilities/ErrorMessage.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+enum ErrorMessage: String {
+    case invalidUsername = "This username created an invalid request. Please try ahain."
+    case unableToComplete = "Unable to complete your request. Plese check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again"
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
