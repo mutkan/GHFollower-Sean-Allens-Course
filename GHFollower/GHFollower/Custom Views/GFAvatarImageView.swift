cd4c12671e4b59b9c7b5016be5db6658ddb0c1dc
Roberto Halgravez <r.halgravez@gmail.com>
2020-02-15 20:22:46 -0700

    Create the GFAvatarImageView (the profile image with corner radius) and this UIImageView subclass now will be used in the FollowersCell

---
+++ GHFollower/GHFollower/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAvatarImageView.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 2/15/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius  = 10
+        clipsToBounds       = true
+        image               = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
