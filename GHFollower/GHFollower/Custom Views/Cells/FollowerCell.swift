a670fab22189ff6a181d8da69f69aaa5130f3505
Roberto Halgravez <r.halgravez@gmail.com>
2020-02-15 20:06:27 -0700

    Create the FollowerCell class for the followers with the profile image and username

---
+++ GHFollower/GHFollower/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,17 @@
+//
+//  FollowerCell.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 2/15/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    
+    let avatarImageView = UIImageView()
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+}
