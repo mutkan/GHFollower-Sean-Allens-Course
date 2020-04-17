8adee23eb33e76708bca12d410a1e2499f3760d4
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-16 20:38:20 -0600

    Create the GFUserInfoHeaderVC (the view controller that will keep the profile information from a user, like username, location, profile image, bio). Create GFSecondaryTitleLabel, already add the code for this class. This label will be use fo the name and location. It has some similarities with GFBodyLabel, but Sean decides to create this specific class rather than one UILabel with several init methods.

---
+++ GHFollower/GHFollower/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 4/16/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+   
+
+}
