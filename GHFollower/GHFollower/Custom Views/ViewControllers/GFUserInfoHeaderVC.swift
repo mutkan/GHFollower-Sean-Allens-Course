0770e6cf75696fc3016d7d7d0a187bd95787c1d3
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-16 20:59:17 -0600

    Add the properties that will be use for each element in the view in GFUserInfoHeader, also created a custom initializer

--- GHFollower/GHFollower/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
+++ GHFollower/GHFollower/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class GFUserInfoHeaderVC: UIViewController {
 
+    let avatarImageView     = GFAvatarImageView(frame: .zero)
+    let usernameLabel       = GFTitleLabel(textAlignment: .left, fontSize: 34)
+    let nameLabel           = GFSecondaryTitleLabel(fontSize: 18)
+    let locationImageView   = UIImageView()
+    let locationLabel       = GFSecondaryTitleLabel(fontSize: 18)
+    let bioLabel            = GFBodyLabel(textAlignment: .left)
+    
+    var user: User!
+    
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
