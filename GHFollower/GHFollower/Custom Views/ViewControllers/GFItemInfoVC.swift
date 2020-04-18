1c562411aa292878116e383b9212defcbb317869
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-17 22:31:20 -0600

    Create the GFItemInfoVC class, this class will have a stack view with the two itemInfoViews and the button, but this class will work as a parent class (because oneGFItemInfoVC is to see the profile and the other one is to get followers, they look similar but do different things)

---
+++ GHFollower/GHFollower/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -0,0 +1,25 @@
+//
+//  GFItemInfoVC.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 4/17/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import UIKit
+
+class GFItemInfoVC: UIViewController {
+    
+    let stackView = UIStackView()
+    let itemInfoViewOne = GFItemInfoView()
+    let itemInfoViewTwo = GFItemInfoView()
+    let actionButton = GFButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
