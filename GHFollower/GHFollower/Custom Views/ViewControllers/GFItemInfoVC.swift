ee8b1bfca68ce0771110d1ecb1a230d47b1815b9
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-17 22:45:35 -0600

    Add the code to define the background color and the position of each element in the GFItemInfoVC

--- GHFollower/GHFollower/Custom Views/ViewControllers/GFItemInfoVC.swift
+++ GHFollower/GHFollower/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -17,9 +17,44 @@ class GFItemInfoVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureBackgroundView()
+        configureStackView()
+        layoutUI()
     }
 
 
+    private func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor    = .secondarySystemBackground
+    }
+    
+    
+    private func configureStackView() {
+        stackView.axis          = .horizontal
+        stackView.distribution  = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoViewOne)
+        stackView.addArrangedSubview(itemInfoViewTwo)
+    }
+    
+    
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
 }
