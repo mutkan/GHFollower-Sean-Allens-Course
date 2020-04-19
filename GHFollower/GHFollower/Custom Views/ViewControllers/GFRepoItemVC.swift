55ec98bd509289546de08c298698ce80f5075635
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-19 01:55:36 -0600

    Create GFRepoItemVC a subclass of GFItemInfoVC (this class will have the repos and the gists). To assign the number of repos and gists, now the superclass needs a custom initializer with a user so the subclasses can have access to public repos, gists, followers, etc

---
+++ GHFollower/GHFollower/Custom Views/ViewControllers/GFRepoItemVC.swift
@@ -0,0 +1,25 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 4/19/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        
+    }
+}
