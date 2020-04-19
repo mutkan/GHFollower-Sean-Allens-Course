a7da8600848f292f2e9a65db255ea04f99fb85d1
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-19 02:03:18 -0600

    Add a set(backgroundColor:title:) method to the GFButton so we can easily set the background and title instead of doing it only in the custom initializer that it has. With his new method we can set the purple color and title for the actionButton (that already has been initialized in the superclass with a generic color and title) in the subclass GFRepoItemVC

--- GHFollower/GHFollower/Custom Views/ViewControllers/GFRepoItemVC.swift
+++ GHFollower/GHFollower/Custom Views/ViewControllers/GFRepoItemVC.swift
@@ -20,6 +20,7 @@ class GFRepoItemVC: GFItemInfoVC {
     private func configureItems() {
         itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
         itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "Github Profile")
         
     }
 }
