a4ea464efe52297de8d56339bb20cf6d257c3711
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-19 02:07:14 -0600

    Create the other subclass GFFollowerItemVC with he followers and following information from the user and the green button with the title "Get Followers"

---
+++ GHFollower/GHFollower/Custom Views/ViewControllers/GFFollowerItemVC.swift
@@ -0,0 +1,25 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 4/19/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            configureItems()
+        }
+        
+        
+        private func configureItems() {
+            itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+            itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+            actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+            
+        }
+    }
