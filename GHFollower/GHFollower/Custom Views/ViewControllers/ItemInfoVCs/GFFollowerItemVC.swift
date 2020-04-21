78692081f1470d00d2391957a52ea7eb00f94340
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-21 01:06:08 -0600

    Fix indentation

--- GHFollower/GHFollower/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
+++ GHFollower/GHFollower/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -9,23 +9,22 @@
 import UIKit
 
 class GFFollowerItemVC: GFItemInfoVC {
-
+    
     override func viewDidLoad() {
-            super.viewDidLoad()
-            configureItems()
-        }
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
         
-        
-        private func configureItems() {
-            itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
-            itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
-            actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
-            
-        }
+    }
     
     
     override func actionButtonTapped() {
         delegate.didTapGetFollowers(for: user)
     }
-    
 }
