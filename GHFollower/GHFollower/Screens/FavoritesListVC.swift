15b9c3e65a5eca321686ddd9887864a62b57ef2c
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 03:34:29 -0600

    Do a clean up in FavoritesListVC and set the correct background color and the correct navigation bar look and title. Also reate the property tableVIew and the array favorites for the followers

--- GHFollower/GHFollower/Screens/FavoritesListVC.swift
+++ GHFollower/GHFollower/Screens/FavoritesListVC.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
+    
+    let tableVIew = UITableView()
+    var favorites = [Follower]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
 
-        view.backgroundColor = .systemBlue
-        
-        
+    }
+    
+    
+    func configureViewController() {
+        view.backgroundColor    = .systemBackground
+        title                   = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    func getFavorites() {
         PersistenceManager.retrieveFavorites { (result) in
             switch result {
             case .success(let favorites):
-                print(favorites)
+                self.favorites = favorites
             case .failure(let error):
                 break
             }
