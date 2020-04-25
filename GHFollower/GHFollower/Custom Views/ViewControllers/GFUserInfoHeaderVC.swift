156531a132fc67f55813428ac54fdc5ee6c9008e
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 14:14:37 -0600

    Refactor the downloadImage(from:) and where it was called.Method and moved to NetworkManager

--- GHFollower/GHFollower/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
+++ GHFollower/GHFollower/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -39,7 +39,7 @@ class GFUserInfoHeaderVC: UIViewController {
     
     
     func configureUIElements() {
-        avatarImageView.downloadImage(from: user.avatarUrl)
+        downloadAvatrImage()
         usernameLabel.text          = user.login
         nameLabel.text              = user.name ?? ""
         locationLabel.text          = user.location ?? "No Location"
@@ -48,6 +48,14 @@ class GFUserInfoHeaderVC: UIViewController {
         
         locationImageView.image     = UIImage(systemName: SFSymbols.location)
         locationImageView.tintColor = .secondaryLabel // To make the location image the same color as the secondaryTitleLabel class
+    }
+    
+    
+    func downloadAvatrImage() {
+        NetworkManager.shared.downloadImage(from: user.avatarUrl) { [weak self] image in
+            guard let self = self else { return }
+            DispatchQueue.main.async { self.avatarImageView.image = image }
+        }
     }
     
     
