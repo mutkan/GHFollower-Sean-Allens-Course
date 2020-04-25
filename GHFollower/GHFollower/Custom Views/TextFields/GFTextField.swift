5fb518161a795ffb1982905e476459f392915807
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 06:10:24 -0600

    Add clearButton while editing (se the x button in the textField)

--- GHFollower/GHFollower/Custom Views/TextFields/GFTextField.swift
+++ GHFollower/GHFollower/Custom Views/TextFields/GFTextField.swift
@@ -36,8 +36,8 @@ class GFTextField: UITextField {
         backgroundColor             = .tertiarySystemBackground
         autocorrectionType          = .no
         returnKeyType               = .go
-        
-        placeholder = "Enter a username"
+        clearButtonMode             = .whileEditing
+        placeholder                 = "Enter a username"
     }
     
 }
