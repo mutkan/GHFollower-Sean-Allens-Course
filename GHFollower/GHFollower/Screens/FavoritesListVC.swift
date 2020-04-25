c355966bb546a13ba054d78b4b130609ec664af8
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 04:49:15 -0600

    Add tableView(_:commit:forRowAt:) method to enable the swipe-to-delete feature of table views. And because we are handling the delete option only, we don't care the others editingStyle. Removes from favorites array and from the table view the corresponding indexPaths

--- GHFollower/GHFollower/Screens/FavoritesListVC.swift
+++ GHFollower/GHFollower/Screens/FavoritesListVC.swift
@@ -93,6 +93,14 @@ extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
         destVC.title = favorite.login
         
         navigationController?.pushViewController(destVC, animated: true)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
         
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
     }
 }
