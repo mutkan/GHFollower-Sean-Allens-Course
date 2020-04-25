2fea50e5214cd67fbb1a29a23e23b71a35b5a21f
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 14:35:34 -0600

    For education purpose, half of the view controllers that are shown need the showLoadingView(), dismissLoadingView() and showEmptyStateView(with:in:). This methods are now part of GFDataLoadingVC. So the view controllers (FollowerListVC and FavoritesListVC) that need this methods are going to be a subclass of GFDataLoadingView. And now the rest of the view controllers don'r have access to this methods (why should they have access to this methods if they don't need them)

---
+++ GHFollower/GHFollower/Custom Views/ViewControllers/GFDataLoadingVC.swift
@@ -0,0 +1,62 @@
+//
+//  GFDataLoadingVC.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 4/25/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import UIKit
+
+class GFDataLoadingVC: UIViewController {
+
+    var containerView: UIView!
+    
+    func showLoadingView() {
+            containerView = UIView(frame: view.bounds)
+            view.addSubview(containerView)
+            
+            containerView.backgroundColor   = .systemBackground
+            containerView.alpha             = 0
+            
+        UIView.animate(withDuration: 0.25) { self.containerView.alpha = 0.8 }
+            
+            let activityIndicator = UIActivityIndicatorView(style: .large)
+            containerView.addSubview(activityIndicator)
+            
+            activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            ])
+            
+            activityIndicator.startAnimating()
+        }
+        
+        
+        func dismissLoadingView() {
+            DispatchQueue.main.async {
+                self.containerView.removeFromSuperview()
+                self.containerView = nil
+            }
+            
+            //Dismiss with animation by James Kinipe in comments
+    //        DispatchQueue.main.async {
+    //            UIView.animate(withDuration: 0.25, animations: {
+    //                containerView.alpha = 0.0
+    //            }) { (true) in
+    //                containerView.removeFromSuperview()
+    //                containerView = nil
+    //            }
+    //        }
+        }
+        
+        
+        func showEmptyStateView(with message: String, in view: UIView) {
+            let emptyStateView = GFEmptyStateView(message: message)
+            emptyStateView.frame = view.bounds
+            view.addSubview(emptyStateView)
+        }
+
+}
--- GHFollower/GHFollower/Screens/FollowerListVC.swift
+++ GHFollower/GHFollower/Screens/FollowerListVC.swift
@@ -12,7 +12,7 @@ protocol FollowerListVCDelegate: class {
     func didRequestFollwers(for username: String)
 }
 
-class FollowerListVC: UIViewController {
+class FollowerListVC: GFDataLoadingVC {
     
     enum Section {
         case main
