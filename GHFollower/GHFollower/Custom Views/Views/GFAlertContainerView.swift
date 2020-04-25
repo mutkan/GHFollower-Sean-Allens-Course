62b086bce77683a0fc53c5d5dc07bb5f30001980
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 06:42:08 -0600

    Move the containerView UI customization to its own UIViewSubclass the view controller (GFAlertVC) doesn't need to know about this

---
+++ GHFollower/GHFollower/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollower
+//
+//  Created by Roberto Halgravez on 4/25/20.
+//  Copyright © 2020 Roberto Halgravez. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        backgroundColor       = .systemBackground
+        layer.cornerRadius    = 16
+        layer.borderWidth     = 2
+        layer.borderColor     = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
