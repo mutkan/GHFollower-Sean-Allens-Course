9a581fec196eb0113ed4b581b1503f0d25166835
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 01:39:57 -0600

    Fix GFErro space (following Sean style)

--- GHFollower/GHFollower/Utilities/GFError.swift
+++ GHFollower/GHFollower/Utilities/GFError.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 enum GFError: String, Error {
-    case invalidUsername = "This username created an invalid request. Please try ahain."
-    case unableToComplete = "Unable to complete your request. Plese check your internet connection."
-    case invalidResponse = "Invalid response from the server. Please try again"
-    case invalidData = "The data received from the server was invalid. Please try again."
-    case unableToFavorites = "There was an error favoriting this user. Please try again."
+    case invalidUsername    = "This username created an invalid request. Please try ahain."
+    case unableToComplete   = "Unable to complete your request. Plese check your internet connection."
+    case invalidResponse    = "Invalid response from the server. Please try again"
+    case invalidData        = "The data received from the server was invalid. Please try again."
+    case unableToFavorites  = "There was an error favoriting this user. Please try again."
     case alreadyInFavorites = "You've already favorited this user.You must REALLY like them!"
 }
