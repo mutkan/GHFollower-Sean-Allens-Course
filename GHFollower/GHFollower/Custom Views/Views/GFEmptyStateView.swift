9f0854da9abcf18b368eef3077413a09e827b132
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 16:41:10 -0600

    Now that we have the DeviceType enum that Sean gave us couple of commits ago, the empty screen when getting the followers can use this enum for the iPhoneSE screen size

--- GHFollower/GHFollower/Custom Views/Views/GFEmptyStateView.swift
+++ GHFollower/GHFollower/Custom Views/Views/GFEmptyStateView.swift
@@ -40,8 +40,15 @@ class GFEmptyStateView: UIView {
         logoImageView.image = Images.emptyStateLogo
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         
+        let labelCenterYConstant: CGFloat = DeviceType.isiphoneSE || DeviceType.isiPhone8Zoomed ? -80 : -150
+        let messageLabelCenterYConstraint = messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: labelCenterYConstant)
+        messageLabelCenterYConstraint.isActive = true
+        
+        let logoBottomConstant: CGFloat = DeviceType.isiphoneSE || DeviceType.isiPhone8Zoomed ? 80 : 40
+        let logoBottomConstraint = logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: logoBottomConstant)
+        logoBottomConstraint.isActive = true
+        
         NSLayoutConstraint.activate([
-            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -140),
             messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
             messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
             messageLabel.heightAnchor.constraint(equalToConstant: 200),
@@ -49,7 +56,6 @@ class GFEmptyStateView: UIView {
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
-            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
         ])
     }
     
