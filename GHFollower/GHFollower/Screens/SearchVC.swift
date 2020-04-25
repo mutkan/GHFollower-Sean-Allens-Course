0a7dabd8a818c9db0e76e6fd5a0d54946b390605
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 06:07:33 -0600

    Remove self, if is not required (like the closures)

--- GHFollower/GHFollower/Screens/SearchVC.swift
+++ GHFollower/GHFollower/Screens/SearchVC.swift
@@ -48,7 +48,7 @@ class SearchVC: UIViewController {
     
     
     func createDismissKeyboardTapGesture() {
-        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
     }
     
