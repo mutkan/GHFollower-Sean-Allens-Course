1f66b24aa1375e9920300c064322d8296d7f94b0
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-25 00:58:57 -0600

    Add the updateWith(favorite:actionType:completed:) method in PersistenceManager to add or delete a follower from the stored favorites array
    Line 25-26: Get the favorites array stored
    Line 27: In case there is a favorites array
        Line 30: See what actionType is required (add or remove the favorite follower)
        Line31-36: See if the stored array already has the follower to add. If already has it, sends an GFError.alreadyInFavorites in completed closure, in case the array doesn't have the follower, add it to the stored array
        Line 37: In remove case: Remove the follower from the stored array if one of the followers in the array contains the same username (login) as the follower to remove
        Line 41: Save the stored array now that was updated
    Line 43: in case there is no favorites array, send GFError.unableToFavorites
    
    Add case .alreadyInFavorites in GFError enum

--- GHFollower/GHFollower/Manager/PersistenceManager.swift
+++ GHFollower/GHFollower/Manager/PersistenceManager.swift
@@ -8,12 +8,44 @@
 
 import Foundation
 
+enum PersistanceActionType {
+    case add, remove
+}
+
 enum PersistenceManager {
     static private let defaults = UserDefaults.standard
     
     enum Keys {
         static let favorites = "favorites"
     }
+    
+    
+    static func updateWith(favorite: Follower, actionType: PersistanceActionType, completed: @escaping (GFError?) -> Void) {
+        
+        retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    guard !retrievedFavorites.contains(favorite) else {
+                        completed(.alreadyInFavorites)
+                        return
+                    }
+                    retrievedFavorites.append(favorite)
+                case .remove:
+                    retrievedFavorites.removeAll { $0.login == favorite.login }
+                }
+                
+                completed(save(favorites: retrievedFavorites))
+                
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
     
     static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
         guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
@@ -29,6 +61,7 @@ enum PersistenceManager {
             completed(.failure(.unableToFavorites))
         }
     }
+    
     
     static func save(favorites: [Follower]) -> GFError? {
         do {
