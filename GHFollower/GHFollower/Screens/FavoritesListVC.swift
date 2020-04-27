9a099ae162da1ca51671bccc630c5f020389ade4
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-26 22:49:58 -0600

    Fix bug when user deletes (or tries to delete) a follower in favorites screen. In tableView(_:commit:forRowAt:) method we are removing the cell first and then removing the follower form the user defaults, but what happens when something goes wrong in the user defaults. You already deleted the user from the table but not in the user defaults. So, moving the delete code from the table view into the else from the guard, when we already successfully delete that same follow from the user defaults, then is when we remove the follower from the table

--- GHFollower/GHFollower/Screens/FavoritesListVC.swift
+++ GHFollower/GHFollower/Screens/FavoritesListVC.swift
@@ -99,13 +99,16 @@ extension FavoritesListVC: UITableViewDelegate, UITableViewDataSource {
         guard editingStyle == .delete else { return }
         
         let favorite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .left)
+        
         
         PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
             guard let self = self else { return }
             
-            guard let error = error else { return }
+            guard let error = error else {
+                self.favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .left)
+                return
+            }
             self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "OK")
             
         }
