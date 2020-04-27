a15e96e546e6f61dde2aeb0a56c464ab38d07bb2
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-26 21:43:04 -0600

    Move the UserInfoVCDelegate and rename it to ItemInfoVCDelegate Now ii located in GFItemInfoVC, why? Because it has more sense to be in this file because the UserInfoVC was the class that conform to this delegate and the one who has the delegate property is GFItemInfoVC.

--- GHFollower/GHFollower/Screens/UserInfoVC.swift
+++ GHFollower/GHFollower/Screens/UserInfoVC.swift
@@ -8,11 +8,6 @@
 
 import UIKit
 
-protocol UserInfoVCDelegate: class {
-    func didTapGitHubProfile(for user: User)
-    func didTapGetFollowers(for user: User)
-}
-
 class UserInfoVC: UIViewController {
     
     let headerView          = UIView()
@@ -115,7 +110,7 @@ class UserInfoVC: UIViewController {
 }
 
 
-extension UserInfoVC: UserInfoVCDelegate {
+extension UserInfoVC: ItemInfoVCDelegate {
     func didTapGitHubProfile(for user: User) {
         guard let url = URL(string: user.htmlUrl) else {
             presentGFAlertOnMainThread(title: "Invalid URL", message: "The url attached to this user is invalid", buttonTitle: "Ok")
