7391ca591c6fc5a044e27054fa0fd9d142db996a
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-26 21:35:55 -0600

    Enable the Dynamic Type Text for the GFBodyLabel with adjustsFontForContentSizeCategory. Also made some value changes for the body section in header to show bios with 3 lines and not just two lines

--- GHFollower/GHFollower/Screens/UserInfoVC.swift
+++ GHFollower/GHFollower/Screens/UserInfoVC.swift
@@ -88,7 +88,7 @@ class UserInfoVC: UIViewController {
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180),
+            headerView.heightAnchor.constraint(equalToConstant:  210),
             
             itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
             itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
