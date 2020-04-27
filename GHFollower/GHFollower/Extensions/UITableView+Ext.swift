54330bb4f4360bbaebb5f50e408b7035a3c587bc
Roberto Halgravez <r.halgravez@gmail.com>
2020-04-26 22:41:45 -0600

    Sean gave us another method for the UITableView extension that he usually use in his projects, but we are not going to use it here because there is just one place where we call reloadData(). But here you have fro you can use it in the future for other projects

--- GHFollower/GHFollower/Extensions/UITableView+Ext.swift
+++ GHFollower/GHFollower/Extensions/UITableView+Ext.swift
@@ -13,4 +13,9 @@ extension UITableView {
     func removeExcessCells() {
         tableFooterView = UIView(frame: .zero)
     }
+    
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async { self.reloadData() }
+    }
 }
